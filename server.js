dcc400f7296773f201c06adfd7871dc59f0dff49
Izaias <izaias@wvtodoz.com.br>
2024-11-19 20:08:41 -0300

    primeira rota criada, pagina inicial, servidor criado

---
+++ server.js
@@ -0,0 +1,16 @@
+//importa o express
+import express from "express";
+
+//atribui tudo oque o express pode fazer para a variavel app
+const app = express();
+
+//subir o servidor
+app.listen(3001, () => {
+    console.log("Servidor ecutando...");
+    // res.send("Servidor rodando na porta 3000");
+});
+
+//cria uma rota
+app.get("/api", (req, res) => {
+    res.status(200).send("Bem vim à imersão");
+});
